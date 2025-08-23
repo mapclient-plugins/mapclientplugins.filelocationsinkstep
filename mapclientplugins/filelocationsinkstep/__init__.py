a341a7cf50937961e91ffb1cb57e4f6a7f18c88d
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-08-23 19:24:48 +1200

    Set version to 0.5.0.

--- mapclientplugins/filelocationsinkstep/__init__.py
+++ mapclientplugins/filelocationsinkstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__ = 'Richard Christie'
 __stepname__ = 'File Location Sink'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.filelocationsinkstep'
